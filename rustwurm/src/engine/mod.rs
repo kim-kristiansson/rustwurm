bed08916ba7b17df3b1a1d26d4ded1408609dd12
kim-kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2025-12-02 11:02:23 +0100

    Update mod.rs

--- rustwurm/src/engine/mod.rs
+++ rustwurm/src/engine/mod.rs
@@ -5,7 +5,7 @@ mod player;
 mod npc;
 
 pub use game::{Game, GameCommand, GameEvent};
-pub use messages::{ClientMessage, ServerMessage};
+pub use messages::{ClientMessage, ServerMessage, Direction, TileInfo};
 pub use monster::{Monster, MonsterId};
 pub use player::{Player, PlayerId};
-pub use npc::{Npc};
+pub use npc::Npc;
